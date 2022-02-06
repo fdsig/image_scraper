699816bccec7dbff920d3147ede976617ca2a1b5
fdsig <fdesigley@gmail.com>
2022-02-06 09:56:43 +0000

    cleaning args parse

--- main.py
+++ main.py
@@ -4,20 +4,16 @@ from dp_challange_web_scraper import Scraper
 
 
 parser = argparse.ArgumentParser(
-    description='IAQA research software using CUHK-PQ, AVA and IAD')
+    description='DP Challenge web scraper')
 
 
 # meta args-- directing sub process
 parser.add_argument('--api_key', default=None, type=str,
-                    help='api key from hugging_face account')
+                    help='api key for scrapapi')
 parser.add_argument('--get_meta', action='store_true',
                     help='if entered will retrieve site data, can be resumed')
-parser.add_argument('--login', action='store_true',
-                    help='if entered will try to sen .csv')
-parser.add_argument('--make', action='store_true',
-                    help='create_new hf project')
-parser.add_argument('--train', action='store_true',
-                    help='create_new hf project')
+parser.add_argument('--get_images', action='store_true',
+                    help='will store full size images')
 
 args = parser.parse_args()
 
